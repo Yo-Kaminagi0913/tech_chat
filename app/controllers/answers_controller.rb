cf098188350eca056020b9de9b96fba047bc281b
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2024-11-12 10:57:03 +0900

    回答の投稿フォーム実装

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,16 @@
 class AnswersController < ApplicationController
+
+  # def new
+  #   @answer = Answer.new
+  # end
+
   def create
+    answer = Answer.create(answer_params)
+    redirect_to "/questions/#{answer.question.id}"
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:name, :content).merge(question_id: params[:question_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
   resources :questions, only: [:index, :new, :create, :show]
+  resources :questions do
+    resources :answers, only: :create
+  end
 end
