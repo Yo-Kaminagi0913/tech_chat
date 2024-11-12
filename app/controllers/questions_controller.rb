6f87866dfdbf9cf7b821c7d440ecaab4aee97874
Yo-Kaminagi0913 <yamazaki1029@yahoo.co.jp>
2024-11-12 11:19:40 +0900

    質問詳細ページに回答の一覧表示機能を実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,6 +15,7 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
     @answer = Answer.new
+    @answers = @question.answers
   end
 
   private
